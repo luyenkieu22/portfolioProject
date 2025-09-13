ee1e8e7bb2aaf2a80acd2d6ab3b86adaa140dc52
luyenkieu22 <125335391+luyenkieu22@users.noreply.github.com>
2025-09-13 18:21:09 +0700

    Update projects

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -1,132 +1,61 @@
-import portfolioImg01 from "../images/portfolio-01.jpg";
+import portfolioImg01 from "../images/portfolio-01.png";
 import portfolioImg02 from "../images/portfolio-02.jpg";
-import portfolioImg03 from "../images/portfolio-03.jpg";
-import portfolioImg04 from "../images/portfolio-04.jpg";
+import portfolioImg03 from "../images/portfolio-03.png";
+import portfolioImg04 from "../images/portfolio-04.png";
 import portfolioImg05 from "../images/portfolio-05.jpg";
-import portfolioImg06 from "../images/portfolio-06.jpg";
-import portfolioImg07 from "../images/portfolio-07.jpg";
 
 const portfolios = [
   {
     id: "01",
     imgUrl: portfolioImg01,
-    category: "Web Design",
+    category: "Ux",
     title: "Portfolio Website",
     description:
       "This is a website introducing myself, my projects, and my experience.",
     technologies: ["React", "Redux", "Tailwind css", "Reactstrap"],
-    siteUrl: "/",
+    siteUrl: "#",
   },
   {
     id: "02",
-    imgUrl: portfolioImg04,
-    category: "Ux",
+    imgUrl: portfolioImg02,
+    category: "Web Design",
     title: "Personnel management",
     description:
       "Human Resource Management Project A website for managing employees, including those currently working, former employees, and those on leave.",
     technologies: ["React", "Redux toolkit", "Material-UI", ".Net"],
-    siteUrl: "https://luyenkieu-preproject.onrender.com/",
+    siteUrl: "https://github.com/luyenkieu22/preProjects.git",
   },
   {
     id: "03",
-    imgUrl: portfolioImg02,
+    imgUrl: portfolioImg04,
+    category: "Web Design",
+    title: "Todo App",
+    description:
+      "TodoX is a task management application (Todo App) developed to help users track, organize, and optimize their daily activities!",
+    technologies: [
+      "ReactJs",
+      "Tailwind css",
+      "Shadcn-ui",
+      "Node.js",
+      "MongoDB",
+    ],
+    siteUrl: "https://github.com/luyenkieu22/luyenkieu_todoX.git",
+  },
+  {
+    id: "04",
+    imgUrl: portfolioImg03,
     category: "Ux",
     title: "Graduation project",
     description:
       "Construction materials management project allows users to purchase, view statistics, sell at the counter",
-    technologies: ["React", "React Query", "Redux", "Material UI", ".net core", "GraphQL"],
-    siteUrl: "https://ptdn-net7.onrender.com/",
-  },
-  {
-    id: "04",
-    imgUrl: portfolioImg05,
-    category: "Web Design",
-    title: "Landing Page",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    siteUrl: "#",
-  },
-  {
-    id: "05",
-    imgUrl: portfolioImg03,
-    category: "Web Design",
-    title: "Landing Page",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    siteUrl: "#",
-  },
-
-  {
-    id: "06",
-    imgUrl: portfolioImg06,
-    category: "Ux",
-    title: "Online Therapy Website",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    siteUrl: "#",
-  },
-  {
-    id: "07",
-    imgUrl: portfolioImg07,
-    category: "Web Design",
-    title: "Appointment Booking Website",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    siteUrl: "#",
-  },
-  {
-    id: "08",
-    imgUrl: portfolioImg01,
-    category: "Web Design",
-    title: "Finance Technology Website",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    siteUrl: "#",
-  },
-  {
-    id: "09",
-    imgUrl: portfolioImg07,
-    category: "Web Design",
-    title: "Appointment Booking Website",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    siteUrl: "#",
-  },
-  {
-    id: "10",
-    imgUrl: portfolioImg01,
-    category: "Web Design",
-    title: "Finance Technology Website",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    siteUrl: "#",
-  },
-  {
-    id: "11",
-    imgUrl: portfolioImg07,
-    category: "Web Design",
-    title: "Appointment Booking Website",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    siteUrl: "#",
-  },
-  {
-    id: "12",
-    imgUrl: portfolioImg01,
-    category: "Web Design",
-    title: "Finance Technology Website",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    siteUrl: "#",
+    technologies: [
+      "React",
+      "React Query",
+      "Redux",
+      "Material UI",
+      ".net core",
+      "GraphQL",
+    ],
   },
 ];
 
